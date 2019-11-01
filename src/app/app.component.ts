030874b2ba6fb883ac3d1ad87d6de14788c227e0
Karthik Arwin <codedelights@gmail.com>
2019-11-01 11:05:00 +0530

    Initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form-example';
+export class AppComponent implements OnInit {
+  sampleForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.sampleForm = this.fb.group({
+
+    });
+  }
 }
